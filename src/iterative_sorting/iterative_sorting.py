a432ca1d8cc706c55c73f78f9eed6074f34dcbdb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-03 18:09:36 -0400

    implemented count_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,18 +8,20 @@ def swap(arr, i, j):
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr) - 1): #O(n)
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        for index, item in enumerate(arr[cur_index:]):
+        for index, item in enumerate(arr[cur_index:]): #O(n)
             smallest = arr[smallest_index]
             if item < smallest:
                 smallest_index = cur_index + index
         # TO-DO: swap
         swap(arr, cur_index, smallest_index)
     return arr 
+
+#O(n^2)
 
 
 # TO-DO:  implement the Bubble Sort function below
@@ -38,5 +40,23 @@ def bubble_sort( arr ):
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
+    if len(arr) == 0:
+        return []
+    elif min(arr) < 0:
+        return "Error, negative numbers not allowed in Count Sort"
 
-    return arr
+    maximum = max(arr) + 1
+    count = [0] * maximum
+    for i in arr:
+        count[i] += 1
+
+    for i in range(1, len(count)):
+        count[i] = count[i] + count[i-1]
+
+    sorted = [0] * count[-1] 
+
+    for i in arr:
+        sorted[count[i] - 1] = i
+        count[i] -= 1
+        
+    return sorted
