8f82570c2bc95883c4c3348c20ccef661dffd726
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-03 16:00:11 -0400

    part 1 MVP done

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,10 @@
+#helpers 
+
+def swap(arr, i, j):
+    temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
@@ -6,21 +13,29 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
+        for index, item in enumerate(arr[cur_index:]):
+            smallest = arr[smallest_index]
+            if item < smallest:
+                smallest_index = cur_index + index
         # TO-DO: swap
-
-
-
-
-    return arr
+        swap(arr, cur_index, smallest_index)
+    return arr 
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    sorted = False
+    print(arr)
+    while not sorted:
+        swaps = 0
+        print(sorted, swaps)
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                print(arr[i], arr[i+1], 'swapped')
+                swap(arr, i, i+1)
+                swaps += 1
+        if(swaps == 0):
+            sorted = True
     return arr
 
 
